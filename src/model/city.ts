de5c51ad9060ceff850bd00472f4c780926c64ab
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2019-08-19 21:46:07 +0100

    cities

---
+++ src/model/city.ts
@@ -0,0 +1,402 @@
+enum City {
+  // United Arab Emirates
+  ABU_DHABI,
+  // Nigeria
+  ABUJA,
+  // Ghana
+  ACCRA,
+  // Ethiopia
+  ADDIS_ABABA,
+  // Algeria,
+  ALGIERS,
+  // Jordan
+  AMMAN,
+  // Netherlands
+  AMSTERDAM,
+  // Andorra
+  ANDORRA_LA_VELLA,
+  // Turkey
+  ANKARA,
+  // Madagascar
+  ANTANANARIVO,
+  // Samoa
+  APIA,
+  // Turkmenistan
+  ASHGABAT,
+  // Eritrea
+  ASMARA,
+  // Kazakhstan
+  ASTANA,
+  // Paraguay
+  ASUNCION,
+  // Greece
+  ATHENS,
+  // Iraq
+  BAGHDAD,
+  // Azerbaijan
+  BAKU,
+  // Mali
+  BAMAKO,
+  // Brunei
+  BANDAR_SERI_BEGAWAN,
+  // Thailand
+  BANGKOK,
+  // Central African Republic
+  BANGUI,
+  // Gambia
+  BANJUL,
+  // Saint Kitts and Nevis
+  BASSETERRE,
+  // China
+  BEIJING,
+  // Lebanon
+  BEIRUT,
+  // Northern Ireland
+  BELFAST,
+  // Serbia
+  BELGRADE,
+  // Belize
+  BELMOPAN,
+  // Germany
+  BERLIN,
+  // Switzerland
+  BERN,
+  // Kyrgyzstan
+  BISHKEK,
+  // Guinea-Bissau
+  BISSAU,
+  // Colombia
+  BOGOTA,
+  // Brazil
+  BRASILIA,
+  // Slovakia
+  BRATISLAVA,
+  // Congo, Republic of the
+  BRAZZAVILLE,
+  // Barbados
+  BRIDGETOWN,
+  // Belgium
+  BRUSSELS,
+  // Romania
+  BUCHAREST,
+  // Hungary
+  BUDAPEST,
+  // Argentina
+  BUENOS_AIRES,
+  // Egypt
+  CAIRO,
+  // Australia
+  CANBERRA,
+  // Venezuela
+  CARACAS,
+  // Wales
+  CARDIFF,
+  // Saint Lucia
+  CASTRIES,
+  // French Guiana
+  CAYENNE,
+  // Moldova
+  CHISINAU,
+  // Sri Lanka
+  COLOMBO14,
+  // Guinea
+  CONAKRY,
+  // Denmark
+  COPENHAGEN,
+  // Senegal
+  DAKAR,
+  // Syria
+  DAMASCUS,
+  // Bangladesh
+  DHAKA,
+  // East Timor
+  DILI,
+  // Djibouti
+  DJIBOUTI,
+  // Tanzania
+  DODOMA15,
+  // Qatar
+  DOHA,
+  // Ireland
+  DUBLIN,
+  // Tajikistan
+  DUSHANBE,
+  // Scotland
+  EDINBURGH,
+  // Sierra Leone
+  FREETOWN,
+  // Tuvalu
+  FUNAFUTI16,
+  // Botswana
+  GABORONE,
+  // Guyana
+  GEORGETOWN,
+  // Burundi
+  GITEGA3,
+  // Guatemala
+  GUATEMALA_CITY,
+  // Vietnam
+  HANOI,
+  // Zimbabwe
+  HARARE,
+  // Cuba
+  HAVANA,
+  // Finland
+  HELSINKI,
+  // Solomon Islands
+  HONIARA,
+  // Pakistan
+  ISLAMABAD,
+  // Indonesia
+  JAKARTA,
+  // South Sudan
+  JUBA,
+  // Afghanistan
+  KABUL,
+  // Uganda
+  KAMPALA,
+  // Nepal
+  KATHMANDU,
+  // Sudan
+  KHARTOUM,
+  // Ukraine
+  KIEV,
+  // Rwanda
+  KIGALI,
+  // Jamaica
+  KINGSTON,
+  // Saint Vincent and the Grenadines
+  KINGSTOWN,
+  // Congo, Democratic Republic of the
+  KINSHASA,
+  // Malaysia
+  KUALA_LUMPUR,
+  // Kuwait
+  KUWAIT_CITY,
+  // Bolivia
+  LA_PAZ,
+  // Gabon
+  LIBREVILLE,
+  // Malawi
+  LILONGWE,
+  // Peru
+  LIMA,
+  // Portugal
+  LISBON,
+  // Slovenia
+  LJUBLJANA,
+  // Togo
+  LOME,
+  // England
+  LONDON,
+  // Angola
+  LUANDA,
+  // Zambia
+  LUSAKA,
+  // Luxembourg
+  LUXEMBOURG,
+  // Spain
+  MADRID,
+  // Marshall Islands
+  MAJURO,
+  // Equatorial Guinea
+  MALABO,
+  // Maldives
+  MALE,
+  // Nicaragua
+  MANAGUA,
+  // Bahrain
+  MANAMA,
+  // Philippines
+  MANILA,
+  // Mozambique
+  MAPUTO,
+  // Lesotho
+  MASERU,
+  // Eswatini (Swaziland)6
+  MBABANA7,
+  // Palau
+  MELEKEOK,
+  // Mexico
+  MEXICO_CITY,
+  // Belarus
+  MINSK,
+  // Somalia
+  MOGADISHU,
+  // Monaco
+  MONACO,
+  // Liberia
+  MONROVIA,
+  // Uruguay
+  MONTEVIDEO,
+  // Comoros
+  MORONI,
+  // Russia
+  MOSCOW,
+  // Oman
+  MUSCAT,
+  // Chad
+  N_DJAMENA, // N'DJAMENA
+  // Kenya
+  NAIROBI,
+  // Bahamas
+  NASSAU,
+  // Myanmar (Burma)
+  NAY_PYI_TAW,
+  // India
+  NEW_DELHI,
+  // Niger
+  NIAMEY,
+  // Cyprus
+  NICOSIA,
+  // Nauru
+  // NO_OFFICIAL_CAPITAL,
+  // Mauritania
+  NOUAKCHOTT,
+  // Tonga
+  NUKU_ALOFA, // NUKU'ALOFA
+  // Norway
+  OSLO,
+  // Canada
+  OTTAWA,
+  // Burkina Faso
+  OUAGADOUGOU,
+  // Federated States of Micronesia
+  PALIKIR,
+  // Panama
+  PANAMA_CITY,
+  // Suriname
+  PARAMARIBO,
+  // France
+  PARIS,
+  // Cambodia
+  PHNOM_PENH,
+  // Montenegro
+  PODGORICA,
+  // Haiti
+  PORT_AU_PRINCE,
+  // Mauritius
+  PORT_LOUIS,
+  // Papua New Guinea
+  PORT_MORESBY,
+  // Trinidad and Tobago
+  PORT_OF_SPAIN,
+  // Vanuatu
+  PORT_VILA,
+  // Benin
+  PORTO_NOVO,
+  // Czech Republic (Czechia)5
+  PRAGUE,
+  // Cape Verde
+  PRAIA,
+  // South Africa
+  PRETORIA, BLOEMFONTEIN, CAPE_TOWN,
+  // Kosovo
+  PRISTINA,
+  // North Korea
+  PYONGYANG,
+  // Ecuador
+  QUITO,
+  // Morocco
+  RABAT,
+  // Iceland
+  REYKJAVIK,
+  // Latvia
+  RIGA,
+  // Saudi Arabia
+  RIYADH,
+  // Italy
+  ROME,
+  // Dominica
+  ROSEAU,
+  // Grenada
+  SAINT_GEORGE_S, // SAINT_GEORGE'S
+  // Antigua and Barbuda
+  SAINT_JOHN_S, // SAINT JOHN'S
+  // Costa Rica
+  SAN_JOSE,
+  // San Marino
+  SAN_MARINO,
+  // El Salvador
+  SAN_SALVADOR,
+  // Yemen
+  SANA_A, //SANA'A
+  // Chile
+  SANTIAGO,
+  // Dominican Republic
+  SANTO_DOMINGO,
+  // Sao Tome and Principe
+  SAO_TOME,
+  // Bosnia and Herzegovina
+  SARAJEVO,
+  // South Korea
+  SEOUL,
+  // Singapore
+  SINGAPORE,
+  // North Macedonia (Macedonia)12
+  SKOPJE,
+  // Bulgaria
+  SOFIA,
+  // Sweden
+  STOCKHOLM,
+  // Fiji
+  SUVA,
+  // Taiwan
+  TAIPEI,
+  // Estonia
+  TALLINN,
+  // Kiribati
+  TARAWA_ATOLL,
+  // Uzbekistan
+  TASHKENT,
+  // Georgia
+  TBILISI,
+  // Honduras
+  TEGUCIGALPA,
+  // Iran
+  TEHRAN,
+  // Israel
+  TEL_AVIV, // (JERUSALEM HAS LIMITED RECOGNITION)
+  // Bhutan
+  THIMPHU,
+  // Albania 
+  TIRANA, // (TIRANE)
+  // Japan
+  TOKYO,
+  // Libya
+  TRIPOLI,
+  // Tunisia
+  TUNIS,
+  // Mongolia
+  ULAANBAATAR,
+  // Liechtenstein
+  VADUZ,
+  // Malta
+  VALLETTA,
+  // Vatican City
+  VATICAN_CITY,
+  // Seychelles
+  VICTORIA,
+  // Austria
+  VIENNA,
+  // Laos
+  VIENTIANE,
+  // Lithuania
+  VILNIUS,
+  // Poland
+  WARSAW,
+  // United States
+  WASHINGTON_DC, // WASHINGTON D.C.
+  // New Zealand
+  WELLINGTON,
+  // Namibia
+  WINDHOEK,
+  // Côte d'Ivoire (Ivory Coast)
+  YAMOUSSOUKRO4,
+  // Cameroon
+  YAOUNDE,
+  // Armenia
+  YEREVAN,
+  // Croatia
+  ZAGREB
+}
